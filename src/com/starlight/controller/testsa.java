8a06e26f568d768495d5dbd28ce96efc9866a8e8
“ywanghuayu <thomas.wang@starlight-sms.com>
2016-12-15 17:29:50 +0800

    demo2

--- src/com/starlight/controller/testsa.java
+++ src/com/starlight/controller/testsa.java
@@ -5,6 +5,6 @@ package com.starlight.controller;
  */
 public class testsa {
     void test(){
-        System.out.println("agaaaaaaaaaaaga ");
+        System.out.println("agaaaaaaaaaaaaaaaga ");
     }
 }
