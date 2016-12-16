42a9b6f25b1056e82a6a6bc34fb95a73f5acb871
“ywanghuayu <thomas.wang@starlight-sms.com>
2016-12-16 14:41:14 +0800

    pull   包名

--- src/com/starlight/controller/test.java
+++ src/com/starlight/controller/test.java
@@ -1,6 +1,5 @@
 package com.starlight.Controll;
 
-import com.starlight.dao.AdminfoEtDao;
 import com.starlight.entity.AdminfoEt;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
